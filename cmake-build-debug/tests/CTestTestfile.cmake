0fcfc51e6f6d031a2defac53b08aa444008d2cd3
YangYzzzz <137391849+YangYzzzz@users.noreply.github.com>
2024-05-06 11:34:11 +0800

    Delete cmake-build-debug directory

--- cmake-build-debug/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Users/CompileEngineeringAssistant/ToLangCompiler/tolangc/tests
-# Build directory: D:/Users/CompileEngineeringAssistant/ToLangCompiler/tolangc/cmake-build-debug/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(TestAll "D:/Users/CompileEngineeringAssistant/ToLangCompiler/tolangc/cmake-build-debug/tests/tests.exe")
-set_tests_properties(TestAll PROPERTIES  _BACKTRACE_TRIPLES "D:/Users/CompileEngineeringAssistant/ToLangCompiler/tolangc/tests/CMakeLists.txt;8;add_test;D:/Users/CompileEngineeringAssistant/ToLangCompiler/tolangc/tests/CMakeLists.txt;0;")
