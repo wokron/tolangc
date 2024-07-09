1dc2cbd2a22fe9c4024e2683b8c9711bca152a4a
20374272 <20374272@buaa.edu.cn>
2024-07-09 17:16:40 +0800

    [feat](mips): print

--- src/mips/mips_manager.cpp
+++ src/mips/mips_manager.cpp
@@ -24,18 +24,6 @@ MipsManager::MipsManager() {
     f12 = new FloatReg(12);
 }
 
-void MipsManager::PrintMips() {
-    AsmWriterPtr writer = AsmWriter::New(_out);
-    for (auto data: datas) {
-        data->PrintData(writer);
-    }
-
-    for (auto code: codes) {
-        code->PrintCode(writer);
-    }
-
-}
-
 void MipsManager::addAsciiz(std::string value) {
     addData(new AsciizData("str" + std::to_string(asciizCount++), value));
 }
