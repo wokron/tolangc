dc0179641052d56222608316eaf37fe13ff23285
StringCat <stringcatwok@gmail.com>
2024-08-24 15:58:46 +0800

    增加集成测试脚本 (#50)
    * 增加集成测试脚本
    
    * 更新脚本

---
+++ scripts/build.sh
@@ -0,0 +1,53 @@
+# This script is used to build tolangc compiler with options.
+
+# Usage: ./build.sh [llvm|pcode]
+
+# Check if the number of arguments is correct
+if [ "$#" -ne 1 ]; then
+    echo "Usage: ./build.sh [llvm|pcode]"
+    exit 1
+fi
+
+# Check if the argument is valid
+if [ "$1" != "llvm" ] && [ "$1" != "pcode" ]; then
+    echo "Invalid argument! Use [llvm|pcode]."
+    exit 1
+fi
+
+backend=$1
+
+# Check required tools (cmake, make, clang)
+if ! command -v cmake &>/dev/null; then
+    echo "Command cmake not found! Please install CMake."
+    exit 1
+fi
+if ! command -v make &>/dev/null; then
+    echo "Command make not found! Please install Make."
+    exit 1
+fi
+if ! command -v clang &>/dev/null; then
+    echo "Command clang not found! Please install Clang."
+    exit 1
+fi
+
+# Check if BUILD_DIR env variable is set
+if [ -z "$BUILD_DIR" ]; then
+    BUILD_DIR="build"
+fi
+
+# Create build directory if it doesn't exist
+if [ ! -d $BUILD_DIR ]; then
+    mkdir -p $BUILD_DIR
+fi
+
+# Build the compiler
+opt=""
+if [ "$backend" = "pcode" ]; then
+    opt="-DPCODE_BACKEND=ON"
+fi
+
+echo $BUILD_DIR
+
+cmake $opt -B$BUILD_DIR .
+
+make -C $BUILD_DIR
